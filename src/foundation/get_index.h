7e212330a9e597b98d955a7ab906f4c13153fd31
Galaxysplash <143564354+Galaxysplash@users.noreply.github.com>
2025-03-03 22:37:45 +0100

    ...

--- src/foundation/get_index.h
+++ src/foundation/get_index.h
@@ -4,5 +4,3 @@
 #include <string_view>
 
 void get_element_for_index_in_string_view_iterator(const std::initializer_list<std::string_view> &ini_list, std::string_view& buffer, unsigned char index);
-
-
