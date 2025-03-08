c81709537b5f873b195dbb42f8a5110458bd254c
Galaxysplash <143564354+Galaxysplash@users.noreply.github.com>
2025-03-08 11:32:00 +0100

    ...

--- src/language/jit_components/tree.cpp
+++ src/language/jit_components/tree.cpp
@@ -1,14 +1,13 @@
 #include "tree.h"
 #include "globals/global_variables.h"
-#include "globals/macros.h"
 
 void tree::exec() {
 }
 
 void tree::build(
     const instruction &instruction_in,
-    const std::initializer_list<std::initializer_list<std::string_view>> && ops_priority_list_move,
-    const std::initializer_list<std::initializer_list<std::string_view>> && keyword_priority_list_move,
+    const PRIORITY_LIST && ops_priority_list_move,
+    const PRIORITY_LIST && keyword_priority_list_move,
     const bool in_terminal
 ) {
     for (const std::initializer_list<std::string_view> & ops_priority_ref : ops_priority_list_move) {
--- src/language/jit_components/tree.h
+++ src/language/jit_components/tree.h
@@ -3,14 +3,16 @@
 #include <initializer_list>
 #include <string_view>
 
+#include "globals/macros.h"
+
 struct instruction;
 
 struct tree {
     static void exec();
     static void build(
         const instruction &instruction_in,
-        const std::initializer_list<std::initializer_list<std::string_view>> && ops_priority_list_move,
-        const std::initializer_list<std::initializer_list<std::string_view>> && keyword_priority_list_move,
+        const PRIORITY_LIST && ops_priority_list_move,
+        const PRIORITY_LIST && keyword_priority_list_move,
         bool in_terminal
     );
 };
